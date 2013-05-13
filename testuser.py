247ac7240496606b185e83845a65c0b056ad15ce
Daniel Fackrell <dfackrell@bluehost.com>
2013-05-13 21:58:08 +0000

    Added a debug message if we don't get passwords

--- testuser.py
+++ testuser.py
@@ -1,3 +1,7 @@
+'''
+Manage Bluehost internal user logins
+'''
+
 import json
 import memcache
 import requests
@@ -9,6 +13,8 @@ api_url = 'https://roster.bluehost.com/cgi-bin/admin/selenium_test_user'
 
 
 class User(object):
+    '''The user object that will log in.'''
+
     def __init__(self, selenium, username=None):
         if username is None:
             username = 'seleniumcron'
@@ -18,14 +24,22 @@ class User(object):
         self.web_pass = self.get_passwords()['web_pass']
 
     def get_passwords(self):
+        '''
+        Pull the passwords from memcache or set new ones
+        '''
         mc = memcache.Client([memcache_server])
         passwords = mc.get(key)
         if passwords is None:
             passwords = self.set_passwords()
             mc.set(key, passwords)
+        if not 'web_pass' in passwords:
+            print('Missing passwords.  We got: ', passwords)
         return json.loads(passwords)
 
     def set_passwords(self):
+        '''
+        Ask for new passwords to be set
+        '''
         args = {
             'user': self.username,
             'method': 'reset_pass',
@@ -35,6 +49,9 @@ class User(object):
         return response.text
 
     def login(self):
+        '''
+        Log in to all three clusters
+        '''
         protocol = 'https://'
         page = '/cgi/admin/memcache'
         for site in ('www.bluehost.com', 'i.bluehost.com', 'my.bluehost.com'):
@@ -43,6 +60,9 @@ class User(object):
             self.login_single_page()
 
     def login_single_page(self):
+        '''
+        Log in at the current page
+        '''
         find_by_css = self.selenium.find_element_by_css_selector
         user_field = find_by_css('.login_username input')
         pass_field = find_by_css('.login_password input')
@@ -50,7 +70,6 @@ class User(object):
         user_field.send_keys(self.username)
         pass_field.send_keys(self.web_pass)
         login_button.click()
-
 
 
 if __name__ == '__main__':
